208e29dd7e542d2b6560eb34846cbe7f3248fd57
fj <1326372778@qq.com>
2019-02-09 14:54:08 +0800

    [feat] 完成x1-x9的计算和测试

--- SVM/src/calculate.py
+++ SVM/src/calculate.py
@@ -1,9 +1,10 @@
 import numpy as np
 import math
 
+
 # 计算 x1
-# @param ： yes_open_price_day ：昨日开盘价
-# @param ：close_price_k ：今日5分钟k线收盘价(某一个值)
+# @param ：yes_open_price_day ：昨日开盘价
+# @param ：close_price_k ：今日5分钟k线最新收盘价
 # @return ：x1  昨日开盘价 / 最新收盘价 - 1
 def calculate_x1(yes_open_price_day, close_price_k):
     return yes_open_price_day / close_price_k - 1
@@ -11,34 +12,43 @@ def calculate_x1(yes_open_price_day, close_price_k):
 
 # 计算 x2
 # @param ：yes_close_price_day ：昨日收盘价
-# @param ：close_price_k ：今日5分钟k线收盘价(某一个值)
+# @param ：close_price_k ：今日5分钟k线最新收盘价
 # @return ：x2  昨日收盘价 / 最新收盘价 - 1
 def calculate_x2(yes_close_price_day, close_price_k):
     return yes_close_price_day / close_price_k - 1
 
 
 # 计算 x3
-# @param ： yes_high_price_day ：昨最高价
-# @param ：close_price_k ：今日5分钟k线收盘价(某一个值)
-# @return ：x3  昨日最高价/最新收盘价-1
+# @param ：yes_high_price_day ：昨最高价
+# @param ：close_price_k ：今日5分钟k线最新收盘价
+# @return ：x3  昨日最高价 / 最新收盘价 - 1
 def calculate_x3(yes_high_price_day, close_price_k):
     return yes_high_price_day / close_price_k - 1
 
 
 # 计算 x4
 # @param ：yes_low_price_day ：昨日最低价
-# @param ：close_price_k ：今日5分钟k线收盘价(某一个值)
-# @return ：x4  昨日最低价/最新收盘价-1
+# @param ：close_price_k ：今日5分钟k线最新收盘价
+# @return ：x4  昨日最低价 / 最新收盘价 - 1
 def calculate_x4(yes_low_price_day, close_price_k):
     return yes_low_price_day / close_price_k - 1
 
 
+# 计算 最新成交量 ： 最新成交量 = 今日最新累计成交量 * 日总k线数 / 开盘至今当前k线数
+# @param ：new_volume_k ：今日最新累计成交量
+# @param ：ri_zong_k_num ：日总k线数
+# @param ：new_k_num ：开盘至今当前k线数
+# @return ：最新成交量
+def calculate_new_volume(new_volume_k, ri_zong_k_num, new_k_num):
+    return new_volume_k * ri_zong_k_num / new_k_num
+
+
 # 计算 x5
 # @param ：yes_volume ：昨日成交量
-# @param ：volume_k ：今日5分钟k线成交量(某一个值)
-# @return ：x5  最新成交量/昨日成交量-1
-def calculate_x5(yes_volume, volume_k):
-    return volume_k / yes_volume - 1
+# @param ：new_volume_k ：今日5分钟k线最新成交量 ： 最新成交量 = 今日最新累计成交量 * 日总k线数 / 开盘至今当前k线数
+# @return ：x5  最新成交量 / 昨日成交量 - 1
+def calculate_x5(yes_volume, new_volume_k):
+    return new_volume_k / yes_volume - 1
 
 
 # 计算昨日波动率
@@ -60,14 +70,32 @@ def calculate_new_bo_dong_lv(new_close_price_k, ri_zong_k_num, new_k_num):
 # 计算 x6
 # @param ：yes_bo_dong_lv ： 昨日波动率
 # @param ：new_bo_dong_lv ： 最新波动率
-# @return ：x6  最新波动率/昨日波动率-1
-def calculate_x6(yes_bo_dong_lv,new_bo_dong_lv):
+# @return ：x6  最新波动率 / 昨日波动率 - 1
+def calculate_x6(yes_bo_dong_lv, new_bo_dong_lv):
     return new_bo_dong_lv / yes_bo_dong_lv - 1
 
 
 # 计算 x7
 # @param ：today_open_price_day ：今日开盘价
 # @param ：new_close_price_k ： 最新收盘价
-# @return ： x7 ： 今日开盘价/最新收盘价-1
+# @return ： x7 ： 今日开盘价 / 最新收盘价 - 1
 def calculate_x7(today_open_price_day, new_close_price_k):
     return today_open_price_day / new_close_price_k - 1
+
+
+# 计算 x8
+# @param ： new_high_price_k ：今日5分钟k线最高价
+# @param ：new_close_price_k ：今日5分钟k线最新收盘价
+# @return ： x8 ：最新最高价 / 最新收盘价 - 1
+def calculate_x8(new_high_price_k, new_close_price_k):
+    return new_high_price_k / new_close_price_k - 1
+
+
+# 计算 x9
+# @param ： new_low_price_k ：今日5分钟k线最新最低价
+# @param ：new_close_price_k ：今日5分钟k线最新收盘价
+# @return ： x9 ： 最新最低价 / 最新收盘价 - 1
+def calculate_x9(new_low_price_k, new_close_price_k):
+    return new_low_price_k / new_close_price_k - 1
+
+
--- SVM/test/test_calculate.py
+++ SVM/test/test_calculate.py
@@ -1,48 +1,63 @@
-import src.calculate as my_test
+import src.calculate as test_calculate
 
 
 def test_calculate_x1():
-    result = my_test.calculate_x1(10, 8)
+    result = test_calculate.calculate_x1(10, 8)
     print("test_result : " + str(result) + " except_result : 0.25")
 
 
 def test_calculate_x2():
-    result = my_test.calculate_x2(10, 8)
+    result = test_calculate.calculate_x2(10, 8)
     print("test_result : " + str(result) + " except_result : 0.25")
 
 
 def test_calculate_x3():
-    result = my_test.calculate_x3(10, 8)
+    result = test_calculate.calculate_x3(10, 8)
     print("test_result : " + str(result) + " except_result : 0.25")
 
 
 def test_calculate_x4():
-    result = my_test.calculate_x4(10, 8)
+    result = test_calculate.calculate_x4(10, 8)
     print("test_result : " + str(result) + " except_result : 0.25")
 
 
+def test_calculate_new_volume():
+    result = test_calculate.calculate_new_volume(10, 10, 5)
+    print("test_result : " + str(result) + " except_result : 20")
+
+
 def test_calculate_x5():
-    result = my_test.calculate_x5(10, 8)
+    result = test_calculate.calculate_x5(10, 8)
     print("test_result : " + str(result) + " except_result : -0.2")
 
 
 def test_calculate_yes_bo_dong_lv():
     yes_close_price = [1, 2, 3]
-    result = my_test.calculate_yes_bo_dong_lv(yes_close_price)
-    print("test_result : " + str(result) + " except_result : (根号)6/3（1.41）")
+    result = test_calculate.calculate_yes_bo_dong_lv(yes_close_price)
+    print("test_result : " + str(result) + " except_result : (根号)6/3（0.8....）")
 
 
 def test_calculate_new_bo_dong_lv():
     new_close_price = [1, 2, 3]
-    result = my_test.calculate_new_bo_dong_lv(new_close_price, 9, 3)
-    print("test_result : " + str(result) + " except_result : 2.4....")
+    result = test_calculate.calculate_new_bo_dong_lv(new_close_price, 9, 3)
+    print("test_result : " + str(result) + " except_result : 1.4....")
 
 
 def test_calculate_x6():
-    result = my_test.calculate_x6(8, 10)
+    result = test_calculate.calculate_x6(8, 10)
     print("test_result : " + str(result) + " except_result : 0.25")
 
 
 def test_calculate_x7():
-    result = my_test.calculate_x7(10, 8)
+    result = test_calculate.calculate_x7(10, 8)
     print("test_result : " + str(result) + " except_result : 0.25")
+
+
+def test_calculate_x8():
+    result = test_calculate.calculate_x8(8, 10)
+    print("test_result : " + str(result) + " except_result : -0.2")
+
+
+def test_calculate_x9():
+    result = test_calculate.calculate_x8(8, 10)
+    print("test_result : " + str(result) + " except_result : -0.2")
