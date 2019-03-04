326e54066e81af1dfc9badf62c5ada1ed8024a32
Fpromiss <1326372778@qq.com>
2019-03-04 19:19:31 +0800

    [fix] 修正按天读取因子数据

--- InversionFactor/main.py
+++ InversionFactor/main.py
@@ -9,14 +9,15 @@ def inversion_factor_calculate(data_path, n):
     all_data = pd.read_csv(data_path)
     # 清洗空数据
     clear_data = all_data.dropna(axis=0, how='any', inplace=False)
-    # 获取所有code
-    code_list = clear_data['code'].groupby(clear_data['code']).first()
-    for code in code_list:
-        print(code)
-        # 获取当前code的所有相关数据
-        code_data = clear_data[clear_data['code'] == code]
-        print(code_data)
-        break
+    # 获取所有日期
+    date_list = clear_data['trade_dt'].groupby(clear_data['trade_dt']).first()
+    date_list = date_list.values
+    # 针对每一天（针对因子计算应该从第n+1天开始）
+    for i in range(n,len(date_list)):
+        print("today:"+str(date_list[i]))
+        # 获取当前天的 n天前的所有数据
+
+
 
 
 if __name__ == '__main__':
